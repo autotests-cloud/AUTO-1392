b0a20eeee5a6a0747e7a3e4bce8bc1a434b60915
Maksim <shpak.point2010@yandex.ru>
2022-10-14 16:07:41 +0500

    delete timeout

--- src/test/java/web/helpers/DriverSettings.java
+++ src/test/java/web/helpers/DriverSettings.java
@@ -14,7 +14,7 @@ public class DriverSettings {
         Configuration.browser = Project.config.browser();
         Configuration.browserVersion = Project.config.browserVersion();
         Configuration.browserSize = Project.config.browserSize();
-        Configuration.timeout = 10000;
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         ChromeOptions chromeOptions = new ChromeOptions();
 
